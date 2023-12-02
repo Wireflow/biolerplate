a1102acd66ad6b21f15a0404cb3bef4b31911afa
Nader Abdulrub <68450205+nabdulrub@users.noreply.github.com>
2023-12-01 23:01:57 -0500

    Delete src directory

--- src/services/Axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const Axios = axios.create({
-  baseURL: "http://192.168.1.159:3000/api/",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  timeout: 5000,
-});
