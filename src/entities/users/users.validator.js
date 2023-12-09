7823d5b9a3cbfd3ecf578dc8e5e2c47210ae02e5
Nader Abdulrub <nabdulrub16@gmail.com>
2023-12-08 22:27:06 -0500

    fix: User validator to check for an email.

--- src/entities/users/users.validator.js
+++ src/entities/users/users.validator.js
@@ -1,11 +1,11 @@
 import { body } from "express-validator";
 
 export const registerValidator = [
-  body("username", "Cannot be empty").isString().notEmpty(),
+  body("email", "Cannot be empty").isString().notEmpty(),
   body("password", "Cannot be empty").isString().notEmpty(),
 ];
 
 export const signInValidator = [
-  body("username", "Cannot be empty").isString().notEmpty(),
+  body("email", "Cannot be empty").isString().notEmpty(),
   body("password", "Cannot be empty").isString().notEmpty(),
 ];
