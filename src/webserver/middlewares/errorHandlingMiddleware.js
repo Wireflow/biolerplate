46189172349bdeea792ac2b5c54dbc950c9732f0
Nader Abdulrub <nabdulrub16@gmail.com>
2023-12-08 22:24:23 -0500

    refactor: error handling middleware

--- src/webserver/middlewares/errorHandlingMiddleware.js
+++ src/webserver/middlewares/errorHandlingMiddleware.js
@@ -1,10 +1,6 @@
 export default function errorHandlingMiddleware(err, req, res, next) {
-  if (err.statusCode === 404) {
-    // Route not found
-    res.status(404).json({ error: "Route not found. Please check api folder" });
-  } else {
-    // Internal server error
-    console.error(err.stack);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  res.status(err.code || 500).json({
+    name: err.name || "Internal Server Error",
+    message: err.message || "Something went wrong",
+  });
 }
